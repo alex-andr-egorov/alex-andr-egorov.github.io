f436d5c053ef9c840be169628d76ad993a851f91
Alexandr <iehorov1995@gmail.com>
2024-11-10 21:48:40 +0200

    test

--- cat/src/cat-api.js
+++
@@ -1,9 +0,0 @@
-import { eventSelect, fetchCatInfo, fetchBreeds } from "./index.js";
-fetchBreeds();
-eventSelect.addEventListener("change", (event) => {
-  const selectedBreedId = event.target.value;
-  if (selectedBreedId) {
-    // Викликати функцію для виконання запиту за повною інформацією про кота
-    fetchCatInfo(selectedBreedId);
-  }
-});
